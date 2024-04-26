6bb7f94135fea05f8c6b812dae2447675bd4e3ff
Nicholai Nissen <nicholainissen@gmail.com>
2024-04-26 11:36:27 +0200

    docs: add jsdoc for mixins

--- src/ability-consumer.ts
+++ src/ability-consumer.ts
@@ -3,6 +3,13 @@ import { createMongoAbility } from '@casl/ability'
 import type { ExtractSubjectType, MongoQuery, Subject, SubjectRawRule } from '@casl/ability'
 import type { AbilityConsumerMethods, AnyRules, DefaultUser } from './types'
 
+/**
+ * Creates a mixin that services can use to get Abilities for an entity,
+ * from another service that provides the rules.
+ *
+ * @param {string} [service] The name of the service that has CASL Ability Provider mixin. Default 'auth'
+ * @returns Mixin
+ */
 export function createCASLAbilityConsumer<
   TUser extends object = DefaultUser,
   TRules extends SubjectRawRule<string, ExtractSubjectType<Subject>, MongoQuery>[] = AnyRules,
--- src/ability-provider.ts
+++ src/ability-provider.ts
@@ -1,6 +1,12 @@
 import type { Context, ServiceSchema } from 'moleculer'
 import type { AnyRules, DefaultUser } from './types'
 
+/**
+ * Creates a mixin that adds an action to provide CASL rules for other services to query.
+ *
+ * @param rulesFor A function that takes your desired entity and returns raw CASL rules.
+ * @returns Mixin
+ */
 export function createCASLAbilityProvider<
   TUser extends object = DefaultUser,
   TMeta extends object = object,
