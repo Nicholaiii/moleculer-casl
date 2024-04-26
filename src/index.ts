d2f3fcd529f4c020756e8642454bac346ba9a0b1
Nicholai Nissen <nicholainissen@gmail.com>
2024-04-26 11:15:23 +0200

    docs: add module documentation for index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+/**
+ * @module
+ *
+ * This module contains functions to search the database.
+ *
+ * ```ts
+ * import { createCASLAbilityProvider } from "@nicholai/moleculer-casl"
+ * {
+ *  mixins: [ createCASLAbilityProvider(rulesFor) ]
+ * }
+ *
+ * ```
+ */
+
 export * from './ability-provider'
 export * from './ability-consumer'
 export * from './types'
